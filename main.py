f71ae3c6e6d1d6f77264ca346c10aa6dd438a0d9
Henry Nguyen <nguyenhenryh1996@gmail.com>
2020-10-13 19:51:33 -0500

    Added multiple windows

--- main.py
+++ main.py
@@ -6,6 +6,14 @@ from kivy.uix.button import Button
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 
+class MenuWindow(Screen):
+    pass
+
+class TutorialWindow(Screen):
+    pass
+
+class WindowManager(ScreenManager):
+    pass
 
 kv = Builder.load_file("main.kv") 
 
