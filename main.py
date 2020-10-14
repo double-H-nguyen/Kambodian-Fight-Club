cb40d1393a077f29d6f731fc2bf2755ff336b41f
Henry Nguyen <nguyenhenryh1996@gmail.com>
2020-10-13 21:19:59 -0500

    Experiment how to use callback functions for btns

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from kivy.uix.button import Button
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 
+player1_selection = 999
+
+
 class MenuWindow(Screen):
     pass
 
@@ -22,10 +25,20 @@ class TutorialWindow(Screen):
   pass
 
 class StartGameWindow(Screen):
+  def p1_btn_number(self, p1_selection):
+    print(f"You pressed the {p1_selection} button (this function was called in main.py)")
+    global player1_selection 
+    player1_selection = p1_selection
+    print_p1_response()
+
+class P1GuessNumber(Screen):
   pass
 
 class WindowManager(ScreenManager):
   pass
+
+def print_p1_response():
+  print(f"Player 1's response was {player1_selection}")
 
 kv = Builder.load_file("main.kv") 
 class Main(App):
