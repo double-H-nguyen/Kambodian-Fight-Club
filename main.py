9912a7bb037fd70a5abf945af513f9317dbd03f3
Henry Nguyen <nguyenhenryh1996@gmail.com>
2020-10-22 20:32:24 -0500

    Added some windows and made misc changes

--- main.py
+++ main.py
@@ -18,10 +18,13 @@ Builder.load_file("main.kv")
 # GLOBAL VARIABLES
 #############################################
 p1_selection = 999
-p1_sum_selection = 999
-
 p2_selection = 999
+
+p1_sum_selection = 999
 p2_sum_selection = 999
+
+p1_turn = 1
+p2_turn = 0
 
 
 #############################################
@@ -40,6 +43,9 @@ class P1GuessNumber(Screen):
 class P1GuessSum(Screen):
   pass
 
+class GiveToP1(Screen):
+  pass
+
 class GiveToP2(Screen):
   pass
 
@@ -55,7 +61,7 @@ class Player2Win(Screen):
 class ChosenNumber(Screen):
   chosen_number_label = ObjectProperty(None)
   sum = ObjectProperty(None)
-  def on_enter(self, *args):
+  def on_pre_enter(self, *args):
     global p1_selection, p2_selection, p1_sum_selection
     total = p1_selection + p2_selection
     self.chosen_number_label.text = f"Numbers chosen: P1={str(p1_selection)} P2={str(p2_selection)}"
@@ -76,6 +82,7 @@ class Game(ScreenManager):
   def store_p1_sum(self, p1_input):
     global p1_sum_selection
     p1_sum_selection = p1_input
+    self.current = 'give_to_p2'
     print_current_state()
 
   def store_p2_num(self, p2_input):
@@ -84,7 +91,10 @@ class Game(ScreenManager):
     print_current_state()
 
   def store_p2_sum(self, p2_input):
-    pass
+    global p2_sum_selection
+    p2_sum_selection = p2_input
+    self.current = 'give_to_p1'
+    print_current_state()
 
   def check_sum():
     pass
@@ -111,6 +121,8 @@ g.add_widget(MenuWindow(name='menu'))
 g.add_widget(TutorialWindow(name='tutorial'))
 g.add_widget(P1GuessNumber(name='p1_guess_num'))
 g.add_widget(P1GuessSum(name='p1_guess_sum'))
+g.add_widget(P2GuessSum(name='p2_guess_sum'))
+g.add_widget(GiveToP1(name='give_to_p1'))
 g.add_widget(GiveToP2(name='give_to_p2'))
 g.add_widget(Player2Selector(name='p2_select_number'))
 g.add_widget(Player1Win(name='player1_win'))
@@ -123,11 +135,7 @@ g.add_widget(ChosenNumber(name='chosen_number'))
 # HELPER METHODS
 #############################################
 def print_current_state(): # debugging only
-  print(f"P1 choice: {p1_selection}")
-  print(f"P1 sum: {p1_sum_selection}")
-  print(f"P2 choice: {p2_selection}")
-  print(f"P2 sum: {p2_sum_selection}")
-
+  print(f"P1 choice: {p1_selection}, P1 sum: {p1_sum_selection}, P2 choice: {p2_selection}, P2 sum: {p2_sum_selection}")
 
 #############################################
 # STANDARD CODE
